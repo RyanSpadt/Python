93c115db3d79238e2392f149dbdecbc1f18fe573
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-05-13 01:58:42 -0400

    Added app.py file to project
    App.py will act as the heart of the program. Instead of defining our object's properties in the init method of the Item class in item.py, we will pass our object's desired properties through a constructor in main. This allows us to send a request for a different item that has different properties from within app.py.

---
+++ Item-Price-Alert/app.py
@@ -0,0 +1,10 @@
+from models.item import Item
+
+
+url = "https://www.johnlewis.com/2020-apple-ipad-pro-11-inch-a12z-bionic-ios-wi-fi-128gb/p4949052"
+tag_name = "p"
+query = {"class": "price price--large"}
+
+item = Item(url, tag_name, query)
+
+print(item.load_price())
