baa77459c94690185a87358713d6259fa2d5fd77
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-05-14 19:15:07 -0400

    More general methods added to Model superclass
    -Added save_to_db() method to Model class, this method upserts data to the database.
    -Added remove_from_db() method to Model class, this method removes data from our database.
    -Added find_one_by() method to Model class, will search a collection for one item by url.
    -Added get_by_id() method to Model class to search database in a collection for an _id, returns our object that has the _id.
    -Added class level definition of collection to subdue warnings
    -Added blank __init__() method in Model class to subdue warnings in Class (temp fix)

--- Item-Price-Alert/models/model.py
+++ Item-Price-Alert/models/model.py
@@ -6,11 +6,25 @@ from common.database import Database
 
 # metaclass gives this class the ability to use abstract methods.
 class Model(metaclass=ABCMeta):
+    
+    collection = "models"
+
+    def __init__(self, *args, **kwargs):
+        pass
 
     # If we have a model it must have a json method. It is the definition of what our models should contain.
     @abstractmethod
     def json(self):
         raise NotImplementedError
+        
+    # Upserts the database based on unique id "_id", it either inserts a new one
+    # Or if it exists already updates the existing.
+    def save_to_db(self):
+        Database.update(self.collection, {"_id": self._id}, self.json())
+
+    # Removes data from collection based on "_id"
+    def remove_from_db(self):
+        Database.remove(self.collection, {"_id": self._id})
 
     # Will return a set of alert objects
     # For item models call Item.all(); for alert models call Alert.all()
@@ -19,3 +33,18 @@ class Model(metaclass=ABCMeta):
     def all(cls) -> List:
         elements_from_db = Database.find(cls.collection, {})
         return[cls(**elem) for elem in elements_from_db]
+    
+    # Searches database in a collection for an _id and returns that singular object
+    @classmethod
+    def get_by_id(cls, _id: str):
+        return cls.find_one_by("_id", _id)
+
+    # Returns a singular item by searching a collection by url
+    @classmethod
+    def find_one_by(cls, attribute, value): # Item.find_one_by('url', 'https://blank.com
+        return cls(**Database.find_one(cls.collection, {attribute: value}))
+
+    # Returns a list of items by search a collection by url
+    @classmethod
+    def find_many_by(cls, attribute, value):
+        return [cls(**elem) for elem in Database.find(cls.collection, {attribute: value})]
