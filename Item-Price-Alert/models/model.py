ffc34772b3b403d3f77b1bc543bb334ba663281a
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-05-14 17:06:26 -0400

    Add model.py to models Package
    -This class defines an abstract class using the metaclass ABCMeta, we can then enforce the fact that each model must have a json method and define what our models should contain.
    -Moved our all() methods from item.py and alert.py into our model.py file inside of our abstract class Model

---
+++ Item-Price-Alert/models/model.py
@@ -0,0 +1,21 @@
+from abc import ABCMeta, abstractmethod
+from typing import Dict, List
+
+from common.database import Database
+
+
+# metaclass gives this class the ability to use abstract methods.
+class Model(metaclass=ABCMeta):
+
+    # If we have a model it must have a json method. It is the definition of what our models should contain.
+    @abstractmethod
+    def json(self):
+        raise NotImplementedError
+
+    # Will return a set of alert objects
+    # For item models call Item.all(); for alert models call Alert.all()
+    # Errors will exist inside this class because collection is not defined here but in sub classes.
+    @classmethod
+    def all(cls) -> List:
+        elements_from_db = Database.find(cls.collection, {})
+        return[cls(**elem) for elem in elements_from_db]
