a4b730618b312afc84ae1a3aa2c62464af5dab42
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-05-15 21:50:05 -0400

    Clean Up Congestion & Reg Blueprint app.py
    -moved app.route('/') into items.py to clean up congestion in app.py
    -removed render_template, request, json, and item from models imports as they are not used
    -registered blueprint item_blueprint to make new end point to our items.py

--- Item-Price-Alert/app.py
+++ Item-Price-Alert/app.py
@@ -1,30 +1,18 @@
-import json
-
-from flask import Flask, render_template, request
+from flask import Flask
 
 from common.database import Database
-from models.item import Item
+from views.items import item_blueprint
+
 
 app = Flask(__name__)
+
+app.register_blueprint(item_blueprint, url_prefix="/items")
 
 
 # Initializes database before we do any other request with Flask
 @app.before_first_request
 def initialize_database():
     Database.initialize()
-
-
-# Tell endpoint if it receives a POST request to take the data from input fields and process it
-@app.route('/', methods=['GET', 'POST'])
-def new_item():
-    if request.method == 'POST':
-        url = request.form['url']   # Accessing the url field from the form data in the request
-        tag_name = request.form['tag_name']
-        query = json.loads(request.form['query'])
-
-        Item(url, tag_name, query).save_to_db()
-
-    return render_template('new_item.html')
 
 
 if __name__ == '__main__':
