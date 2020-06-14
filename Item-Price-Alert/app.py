a041a1300ce812ba616330f44c3d374bd78b97de
RyanSpadt <60458664+RyanSpadt@users.noreply.github.com>
2020-06-13 20:59:11 -0400

    app.py blueprint for stores and users
    - Import and registration for stores and users blueprint.

--- Item-Price-Alert/app.py
+++ Item-Price-Alert/app.py
@@ -3,11 +3,15 @@ from flask import Flask
 from common.database import Database
 
 from views.alerts import alert_blueprint
+from views.stores import store_blueprint
+from views.users import user_blueprint
 
 
 app = Flask(__name__)
 
 app.register_blueprint(alert_blueprint, url_prefix="/alerts")
+app.register_blueprint(store_blueprint, url_prefix="/stores")
+app.register_blueprint(user_blueprint, url_prefix='/users')
 
 
 # Initializes database before we do any other request with Flask
